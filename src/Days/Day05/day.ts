3a18c157dada45f5372d6fabe2904c55afe1b0e4
Philipp Priller <subNet32@gmail.com>
2023-12-05 19:30:33 +0100

    optimized day5 477s -> 0.005s

--- src/Days/Day05/day.ts
+++ src/Days/Day05/day.ts
@@ -2,10 +2,15 @@ import { StringUtils } from '../../Utils/StringUtils'
 import { DayResult } from '../../types'
 import { Range } from '../../types/numeric/Range'
 
+type TransformationRange = {
+  range: Range
+  transformation: number
+}
+
 type Map = {
   source: string
   target: string
-  ranges: { range: Range; transformation: number }[]
+  ranges: TransformationRange[]
 }
 
 export default function Day(): DayResult {
@@ -52,6 +57,31 @@ export default function Day(): DayResult {
     return value + foundRage.transformation
   }
 
+  function transformRange(ranges: Range[], mapIndex: number, maps: Map[]) {
+    if (mapIndex >= maps.length) return ranges.reduce<number | null>((lowest, range) => (lowest === null || range.start < lowest ? range.start : lowest), null)
+    const map = maps[mapIndex]
+
+    const rangesToCheck: Range[] = [...ranges]
+    const transformedRanges: Range[] = []
+
+    while (rangesToCheck.length) {
+      const range = rangesToCheck.pop() as Range
+      const intersections = map.ranges.map<TransformationRange>((r) => ({ range: r.range.intersection(range) as Range, transformation: r.transformation })).filter((q) => !!q.range)
+      let remainingRanges = [range]
+      intersections.forEach((inter) => {
+        const cutRanges: Range[] = []
+        remainingRanges.forEach((rr) => {
+          cutRanges.push(...rr.cut(inter.range))
+        })
+        remainingRanges = [...cutRanges]
+      })
+      transformedRanges.push(...remainingRanges)
+      transformedRanges.push(...intersections.map((inter) => inter.range.offset(inter.transformation)))
+    }
+
+    return transformRange(transformedRanges, mapIndex + 1, maps)
+  }
+
   async function solve1(input: string[]) {
     const { seeds, maps } = init(input)
 
@@ -73,21 +103,7 @@ export default function Day(): DayResult {
   async function solve2(input: string[]) {
     const { seedRanges, maps } = init(input)
 
-    let lowestLocation: number | null = null
-    seedRanges.forEach((range) => {
-      for (let seed = range.start; seed <= range.end; seed++) {
-        let value = seed
-        // console.log('\nRunning Seed', seed)
-        maps.forEach((map) => {
-          const valueBefore = value
-          value = transformWithMap(value, map)
-          // console.log(valueBefore, '  ', map.source, ' - ', map.target, ' --> ', value)
-        })
-        if (lowestLocation === null || value < lowestLocation) lowestLocation = value
-      }
-    })
-
-    return lowestLocation
+    return transformRange(seedRanges, 0, maps)
   }
 
   return { solve1, solve2 }
--- src/types/numeric/Range.ts
+++ src/types/numeric/Range.ts
@@ -13,16 +13,42 @@ export class Range implements IRange {
     if (end < start) throw new Error(`Invalid Range. '${start}' - '${end}'`)
   }
 
+  public static FromRange(range: IRange) {
+    return new Range(range.start, range.end)
+  }
+
   public inRange(number: number) {
     return number >= this.start && number <= this.end
   }
 
   public intersects(range: Range) {
-    return this.inRange(range.start) || this.inRange(range.end) || range.inRange(this.start) || this.inRange(this.end)
+    return this.inRange(range.start) || this.inRange(range.end) || range.inRange(this.start) || range.inRange(this.end)
   }
 
-  public union(range: Range) {
+  public intersection(range: Range) {
     if (!this.intersects(range)) return null
-    return new Range(Math.min(this.start, range.start), Math.max(this.end, range.end))
+    return new Range(Math.max(this.start, range.start), Math.min(this.end, range.end))
+  }
+
+  public offset(offset: number) {
+    this.start += offset
+    this.end += offset
+    return this
+  }
+
+  public cut(range: Range) {
+    if (!this.intersects(range)) return [Range.FromRange(this)]
+    const result: Range[] = []
+    if (this.start < range.start) {
+      result.push(new Range(this.start, range.start - 1))
+    }
+    if (this.end > range.end) {
+      result.push(new Range(range.end + 1, this.end))
+    }
+    return result
+  }
+
+  public toString() {
+    return `${this.start} - ${this.end}`
   }
 }
