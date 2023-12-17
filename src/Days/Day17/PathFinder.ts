35727b06024fdf173e97951c90b38fda2afd4b86
Philipp Priller <subNet32@gmail.com>
2023-12-17 15:14:07 +0100

    Day17 optimization

--- src/Days/Day17/PathFinder.ts
+++ src/Days/Day17/PathFinder.ts
@@ -1,3 +1,4 @@
+import Heap from 'heap-js'
 import Vector2 from '../../types/2D/Vector2'
 import { Point2D } from '../../types/2D/types2D'
 
@@ -11,42 +12,28 @@ export type PathNode = {
   prevNode: PathNode | null
 } & Node
 
-function compareNodes(nodeA: Node, nodeB: Node) {
-  return Vector2.equals(nodeA.position, nodeB.position) && Vector2.equals(nodeA.direction, nodeB.direction)
-}
-
 function hashNode(node: Node) {
   return Vector2.toString(node.position) + '|' + Vector2.toString(node.direction)
 }
 
 export function PathFinder(startNode: Node, endNode: Node, getAdjacentNodes: (current: PathNode) => Node[]) {
-  const visitedNodes = new Set<string>()
-  let nodesToCheck: PathNode[] = []
+  const addedNodes = new Set<string>()
+  const nodesToCheck = new Heap<PathNode>((a, b) => a.cost - b.cost)
   let currentNode: PathNode = { position: startNode.position, prevNode: null, cost: 0, direction: Vector2.Zero }
+
   do {
-    const adjNodes = getAdjacentNodes(currentNode).filter((n) => !visitedNodes.has(hashNode(n)))
-    visitedNodes.add(hashNode(currentNode))
+    const adjNodes = getAdjacentNodes(currentNode)
 
     if (adjNodes?.length) {
       adjNodes.forEach((node) => {
-        const foundNode = nodesToCheck.find((n) => compareNodes(node, n))
-        if (!foundNode) {
-          nodesToCheck.push({ ...node, prevNode: currentNode })
-          return
-        }
-
-        // found faster path to this node
-        if (node.cost < foundNode.cost) {
-          foundNode.cost = node.cost
-          foundNode.prevNode = currentNode
-        }
+        const nodeKey = hashNode(node)
+        if (addedNodes.has(nodeKey)) return
+        addedNodes.add(nodeKey)
+        nodesToCheck.push({ ...node, prevNode: currentNode })
       })
     }
-    let nextNode: PathNode | null = null
-    for (const nodeToCheck of nodesToCheck) {
-      if (!nextNode || nodeToCheck.cost < nextNode.cost) nextNode = nodeToCheck
-    }
-    nodesToCheck = nodesToCheck.filter((node) => node !== nextNode)
+
+    const nextNode = nodesToCheck.pop()
 
     if (!nextNode) throw new Error('No path found')
     currentNode = nextNode
